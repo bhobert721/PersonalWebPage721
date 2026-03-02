00b3489e70121d6098211f645953b89c4d2967e6
bhobert721 <bhobert@mail.umassglobal.edu>
2026-03-02 10:20:58 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 window.onload = function () {
-  alert("JS is working");
 
   const image = document.getElementById("oceanImage");
 
   if (image) {
     image.src = "https://picsum.photos/400/300";
   } else {
-    alert("Image not found");
   }
 };
