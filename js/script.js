6123b78154f4e2c8970b5402d5e346aa60d473d6
bhobert721 <bhobert@mail.umassglobal.edu>
2026-03-02 09:11:58 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-function getOceanImage() {
+window.onload = function () {
   const image = document.getElementById("oceanImage");
 
-  // Random number prevents caching
   const randomNum = new Date().getTime();
 
   image.src = "https://source.unsplash.com/400x300/?ocean,beach&" + randomNum;
-}
-
-// Load one automatically when page opens
-getOceanImage();
+};
